6bcc9dd7943ec6e22138e8cfac93155c9a9fb59b
mihai-alex <alex.mihai.email@gmail.com>
2023-04-16 08:01:53 +0300

    Add header, footer, routing for empty URL

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfessorListComponent } from './professor-list/professor-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'professors', component: ProfessorListComponent },
+
+  // TODO: possibly add home page component and route:
+  { path: '', redirectTo: 'professors', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
